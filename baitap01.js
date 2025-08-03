f7f4eea26350a741794cbbe90b3bb95f7a64f827
trancanh277 <tranminhcanh.tp@gmail.com>
2025-08-03 22:23:20 +0900

    upload session 16 bai tap 1

---
+++ baitap01.js
@@ -0,0 +1,10 @@
+function processConfirm(answer) {
+    let result ;
+    if(answer) {
+        result = "Excellent. We'll play a nice game of chess.";
+    }
+    else {
+        result = "Maybe later then.";
+    }
+    return result;
+}
